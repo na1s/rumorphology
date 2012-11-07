233198c22f53f05fa75a06d5fca99ce3ca738cb9
na1s <nasyrov@gmail.com>
2012-11-07 21:05:49 +0500

    added tests

---
+++ Rumorphology/Rumorphology.Tests/Lucene/OpenCorporaRussianStemmerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Rumorphology.Lucene;
+using Rumorphology.OpenCorpora;
+using Rumorphology.OpenCorpora.DictionaryProcessor;
+
+namespace Rumorphology.Tests.Lucene
+{
+    [TestFixture]
+    public class OpenCorporaRussianStemmerTests
+    {
+        [Test]
+        public void Test()
+        {
+            var baseFormQuery = new Mock<IWordBaseFormQuery>();
+            baseFormQuery.Setup(b => b.GetLemmas("смеси")).Returns(new List<Lemma>
+                                                                       {
+                                                                           new Lemma
+                                                                               {
+                                                                                   Text = "смесь"
+                                                                               },
+                                                                           new Lemma
+                                                                               {
+                                                                                   Text = "смесить"
+                                                                               }
+                                                                       });
+            var stemmer = new OpenCorporaRussianStemmer(baseFormQuery.Object);
+            Assert.AreEqual(new[] { "смесь", "смесить" }.OrderBy(t => t), stemmer.Stem("смеси").OrderBy(t => t));
+        }
+    }
+}
---
+++ Rumorphology/Rumorphology.Tests/OpenCorpora/DictionaryProcessor/InMemoryWordsDictionaryTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Rumorphology.OpenCorpora;
+using Rumorphology.OpenCorpora.DictionaryProcessor;
+
+namespace Rumorphology.Tests.OpenCorpora.DictionaryProcessor
+{
+    [TestFixture]
+    public class InMemoryWordsDictionaryTests
+    {
+        [Test]
+        public void Test()
+        {
+            var v = new InMemoryWordsDictionary();
+            v.ProcessLemma(new Lemma()
+                               {
+                                   Text = "смесь",
+                                   Forms = new List<LemmaForm>()
+                                               {
+                                                   new LemmaForm("смеси",new List<Grammem>()),
+                                                    new LemmaForm("смесей",new List<Grammem>()),
+                                               }
+                               });
+        }
+    }
+}
